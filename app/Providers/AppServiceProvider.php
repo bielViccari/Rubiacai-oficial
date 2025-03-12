de148af55da286e633c664504971c5a1080c628a
gabriel Viccari <gabriel.viccari20@gmail.com>
2025-03-12 02:51:27 -0300

    to use mysql database 5.6 or lower

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
         if(env('APP_ENV', 'production') == 'production') { // use https only if env is production
             \URL::forceScheme('https');
            }
+        Schema::defaultStringLength(191);
     }
 }
